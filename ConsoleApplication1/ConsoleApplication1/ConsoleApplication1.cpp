a4ed956b3b9eba57b7e771a33fa4c2ca4367e020
antans <antanswork@gmail.com>
2022-06-16 14:06:49 +0300

    Pievienotas 3 jautajumi un to atbildes, ka ari iespeja spelet testu

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,46 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+
+
+int main(){
+
+    string ieraksts;
+    int i = 0;
+    
+
+        
+
+
+
+
+
+
+
+    string jautajumi[] = { "1.Kuri no šiem mainīgo datu tipiem nav skaitliski?\n a)string \n b)int \nc)char\nd)double",
+                            "2.Cik liels ir wChar izmērs bitos \n a)2 \n b)4 \nc)6\nd)8",
+                            "3.Ka pareizi deklaret mainigo \n a)int a,b; \n b)int a;\nint b; \nc)int a int b;\nd)a,b int;"};
+
+    string atbildes[] = { "a,c","a,b","a,b" };
+
+
+
+
+
+    for (i = 0; i < 10; i++) {
+        do {
+
+            cout << jautajumi[i];
+            cout << "\nAtbilde: ";
+            cin >> ieraksts;
+
+
+        } while (ieraksts != atbildes[i]);
+    };
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
