d8649ada807e56cd4adc1b30b16f443c8ef1c78b
antans <antanswork@gmail.com>
2022-06-16 16:28:56 +0300

    Izveidoti visi 10 jautājumi

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -28,15 +28,17 @@ int main(){
                             "2.Cik liels ir wChar izmers bitos \n a)2 \n b)4 \nc)6\nd)8",
                             "3.Ka pareizi deklaret mainigo \n a)int a,b; \n b)int a;\nint b; \nc)int a int b;\nd)a,b int;",
                             "4.Kuri no siem mainigo datu tipiem ir skaitliski?\n a)string \n b)int \nc)char\nd)double",
-                            "5.Kadas var but bool vertibas?\n a)1 un 0\n b)2 un 1 \nc)Yes un No\nd)0 un 1",
+                            "5.Kadas var but bool vertibas?\n a)true un false\n b)2 un 1 \nc)Yes un No\nd)false un true",
                             "6.Kuri ir primarie datu tipi?\n a)int \n b)double \nc)char\nd)array", 
                             "7.Kuri ir sekondarie datu tipi?\n a)int \n b)function \nc)char\nd)array",
-                            "8.Kadas vertibas pieder datu tipam void ? \n a)Null \n b)1 \nc)0\nd)zero",};
+                            "8.Kadas vertibas pieder datu tipam void ? \n a)Null \n b)1 \nc)0\nd)zero",
+                            "9.Ar kadiem modifikatoriem var mainit int datu tipu ? \n a)short \n b)long \nc)small\nd)big",
+                            "10.Ko nedriskt izmantot ka mainiga nosaukumu? \n a)Null \n b)Main \nc)int\nd)kokuSkaits", };
 
 
 
 
-    string atbildes[] = { "a,c","a,b","a,b","b,d","a,d","a,b,c","b,d","a,c"};
+    string atbildes[] = { "a,c","a,b","a,b","b,d","a,d","a,b,c","b,d","a,c","a,b","a,b,c"};
 
     cout << "Tests par Mainigajiem, to datu tipiem, pamatdarbibam, darbam ar tiem programmesanas valoda C++\nJautajuma ir 4 atbildes un 2 vai 3 pareizi varianti\nVeiksmi!\n\n";
 
